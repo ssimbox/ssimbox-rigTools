c855ce0018728bfbe6c21ed9afe48a9620992134
ssimbox <ssimone0825@gmail.com>
2021-03-26 20:37:17 +0100

    fixed constraints

--- Game Hierarchy/gameJointExp.py
+++ Game Hierarchy/gameJointExp.py
@@ -1,21 +1,31 @@
 import maya.cmds as cmds
 
+"""def buttonNumber(*args):
+    count = 0
+    count += 1
+    print(count)"""
+
 def jointMaker(*args):
 
     global suffix_UI
+    global suffix
     global mainJoints
+    global gameJoints
 
     mainJoints = cmds.ls(sl=1, typ="joint")
     suffix = cmds.textField(suffix_UI, q=1, text=True)
     #cmds.textScrollList('listJointUI',e=1, ra=1)
     #cmds.textScrollList('listJointUI', e=1, append=[""+str(mainJoints)+""])
     
-    #Make new '_game' joints, and position them in place.
+    # Make new '_game' joints, and position them in place.
     gameExpGrp = cmds.group(n="gameExp_grp", em=1)
-    
+    print(gameExpGrp)
+    if cmds.objExists(gameExpGrp + "1"):
+        cmds.delete(gameExpGrp)
+
     cmds.select(d=1)
     gameJoints = []
-    
+
     
     for i in mainJoints:
         jnt = cmds.joint(n=i+'_game', rad=0.65)
@@ -23,23 +33,25 @@ def jointMaker(*args):
         # remove suffix on user input
         if suffix in jnt:
             new = str(jnt).replace(suffix, "")
-            jnt = cmds.rename(jnt, new) 
+            jnt = cmds.rename(jnt, new)
             cmds.select(cl=1)
-            print("jnt in if-->", jnt)
+            #print("jnt in if-->", jnt)
         gameJoints.append(jnt)
         cmds.select(d=1)
 
-    #Convert rotations of new joints to joint orient.
+    # Convert rotations of new joints to joint orient.
     for i in gameJoints:
         cmds.makeIdentity(i, apply = True, t=0, r=1, s=0, n=0)
         cmds.parent(i, gameExpGrp)
+        # if cmds.objExists(gameExpGrp):
 
 
     #Parent constraint from rig_jnts to game_jnts
 def makeConstraint(*args):
-    for i in mainJoints:
-        if cmds.objExists(i+'_game'):
-            cmds.parentConstraint(i, i+'_game', mo=1)
+    for i in range(len(mainJoints)):
+        #print (mainJoints[i], gameJoints[i])
+        # if cmds.objExists(i+'_game'):
+        cmds.parentConstraint(mainJoints[i], gameJoints[i], mo=1)
 
 
 def showUI():
@@ -50,7 +62,7 @@ def showUI():
     myWin = cmds.window("gameExportUI", t="gameExportUI", w=300, h=300, s=1)
     mainLayout = cmds.formLayout(nd=50)
 
-    suffix_UI = cmds.textField(w=100, tx="_")
+    suffix_UI = cmds.textField(w=100, tx="_bind")
     jointMaker_btn = cmds.button(l="build joint", c=jointMaker)
     constraintMaker_btn = cmds.button(l="make constraints", c=makeConstraint)
     #listJointUI = cmds.textScrollList("listJointUI",numberOfRows=8)
