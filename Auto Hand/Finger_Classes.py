4bf34ce1f1491de9a59ed6f63ce137a5c8f4eddc
ssimbox <ssimone0825@gmail.com>
2021-07-29 12:17:19 +0200

    polished (?) duplicate fingers chain plus ikHandle

--- Auto Hand/Finger_Classes.py
+++ Auto Hand/Finger_Classes.py
@@ -1,30 +1,50 @@
 import maya.cmds as cmds
 
 class Finger:
-    def __init__(self, chainSel, fullLength):
-        self.chainSel = chainSel #primo elemento della catena
-        self.fullLength = fullLength #l'intera catena
+    def __init__(self, metacarp, distal, fullLength):
+        self.metacarp = metacarp #Metacarp -> First bone in a finger
+        self.distal = distal #Distal -> Last bone in a finger
+        self.fullLength = fullLength #Entire chain
 
     def jointColor(self, colorR, colorG, colorB):
-        cmds.color(self.chainSel, rgb=(colorR, colorG, colorB))
+        cmds.color(self.metacarp, rgb=(colorR, colorG, colorB))
 
-    def ikHandles(self, endJoint, nomone):
-        cmds.ikHandle(sj=self.chainSel, ee=endJoint, sol="ikSCsolver", n=nomone)
+    def ikHandles(self, nomone):
+        cmds.ikHandle(sj=self.metacarpDup, ee=self.distalDup, sol="ikSCsolver", n=nomone)
+    
+    def duplFinger(self, metacarpDup, distalDup):
+        self.metacarpDup = metacarpDup
+        self.distalDup = distalDup
+        pass
+    
+    """potrei dichiarare le variabili di classe di duplFinger dentro __init__ e, nel metodo appena citato 
+        eseguo il processo con cui si creano i duplicati??? boh ci devo provare"""
 
-    def duplicateFingerChain(self): 
-        chain = [] #dichiaro chain come una lista
+    """
+    def duplicateFingerChain(self, firstDup, endDup): 
+        
+        self.firstDup = firstDup
+        self.endDup = endDup
+        firstDup = [] #dichiaro chain come una lista
         suffix = ["_rig"]
-        # print("immediate after append --> ", chain)
+        
         for y in suffix:
-            #carico la lista ad ogni giro del ciclo per poter effettuare tutte le operazioni sulle liste
-            
+
+            #carico la lista ad ogni giro del ciclo per poter effettuare tutte le operazioni sulle liste    
             for x in range(len(fullFinger)):
-                chain.append(fullFinger[x])
-                cmds.joint(n = chain[x] + y, radius = 1)
-                cmds.matchTransform(chain[x] + y, fullFinger[x])
+                
+                firstDup.append(fullFinger[x])
+                cmds.joint(n = firstDup[x] + y, radius = 1)
+                cmds.matchTransform(firstDup[x] + y, fullFinger[x])
                 #cmds.makeIdentity(chain[x] + y, a = 1, t = 0, r = 1, s = 0)
         
-        cmds.parent((chain[0] + "_rig"), world = True)
+        cmds.parent((firstDup[0] + "_rig"), world = True)
+    """
+class Hand:
+    def __init__(self, carpal):
+        self.carpal = carpal 
+        pass
+
 
 startJoint = cmds.ls(sl=1)[0]
 fullFinger = cmds.listRelatives(startJoint, ad=1, typ="joint")
@@ -33,37 +53,38 @@ fullFinger.reverse()
 # print("fullFinger --> ", fullFinger)
 
 # call Finger class
-firstFinger = Finger(startJoint, len(fullFinger))
+# startJoint[0] = first joint selected and so first into the chain 
+# fullFinger[-1] = it is the last joint in the chain
+firstFinger = Finger(fullFinger[0], fullFinger[-1], len(fullFinger))
 
 # duplicate the finger base chain with a new one
-firstFinger.duplicateFingerChain()
+#firstFinger.duplicateFingerChain(startJoint[0] + "_rig")
+
+newJoint = []
+for i in fullFinger:
+    asd = cmds.joint(n=i+"__nuovo")
+    newJoint.append(asd)
+    cmds.matchTransform(asd, i)
+    #print(newJoint[i])
+print(newJoint)
+cmds.parent(newJoint[0], w=1)
+
+# richiamo il metodo della classe dichiarando chi è cosa
+dupFinger = firstFinger.duplFinger(newJoint[0], newJoint[-1])
+
+"""
+rigChain = ["_rig"]
+for y in rigChain:
+    for x in range(len(fullFinger)):
+        asd = cmds.joint(n=fullFinger[x] + y, rad=.7)
+        cmds.matchTransform(fullFinger[x] + y, fullFinger[x])
+"""
 
 # Create ikHandle
-firstFinger.ikHandles(fullFinger[-1], startJoint + "_ikHandle")
+firstFinger.ikHandles(startJoint + "_ikHandle")
 
 # Change color based on side
 if startJoint[0:2] == "l_":
     firstFinger.jointColor(0, 0, 255)
 elif startJoint[0:2] == "r_":
     firstFinger.jointColor(255, 0, 0)
-
-
-"""class Finger:
-    def __init__(self, fullLength, chainSel):
-        self.chainSel = cmds.ls(sl=1)
-        jointsNumber = cmds.listRelatives(self.chainSel, ad=1)
-        jointsNumber.append(chainSel)
-        jointsNumber.reverse()
-        self.fullLength = len(jointsNumber)
-        print (fullLength)
-
-    def jointColor(self, colorR, colorG, colorB):
-        self.colorR = colorR
-        self.colorG = colorG
-        self.colorB = colorB
-        cmds.color(self.chainSel, rgb=(colorR, colorG, colorB))
-
-
-# chainSel = cmds.ls(sl=1)
-colorOne = Finger(0, chainSel)
-colorOne.jointColor(0, 0, 255)"""
