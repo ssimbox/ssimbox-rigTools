2bdb80e235ce2e85c315f12ea67a3549a9c6c96e
ssimbox <ssimone0825@gmail.com>
2021-08-05 12:35:05 +0200

    moving outside class some methods

--- Auto Hand/Finger_Classes.py
+++ Auto Hand/Finger_Classes.py
@@ -7,44 +7,25 @@ class Finger(object):
         self.fullLength = fullLength #Entire chain
 
 class DriverFinger(Finger):
-    def __init__(self, metacarp, distal, fullLength):
+    def __init__(self, metacarp, distal, fullLength, metacarpDrv, distalDrv, hierarchyDrv):
         super(DriverFinger, self).__init__(metacarp, distal, fullLength)
-
-    def duplicateChain(self, metacarpDrv, distalDrv):
-        
+                
         self.metacarpDrv = metacarpDrv
         self.distalDrv = distalDrv
-        self.hierarchyDrv = []
-
-        for x in range(len(self.fullLength)):
-            boneDrv = cmds.joint(n=self.fullLength[x] + "__nuovo")
-            self.hierarchyDrv.append(boneDrv) 
-            cmds.matchTransform(boneDrv, self.fullLength[x])
-               
-        cmds.parent(metacarpDrv, w=1) #Parent into world the driver chain
-        cmds.select(cl=1) #Maya automatically selects the new chain but I don't want
+        self.hierarchyDrv = hierarchyDrv
     
     def connectToOrigin(self):
-        for x in range(len(self.fullLength)):
-            cmds.parentConstraint(self.hierarchyDrv[x], self.fullLength[x])
+        for boneDrv, bone in zip(self.hierarchyDrv,self.fullLength):
+            cmds.parentConstraint(boneDrv, bone)
 
-    def ikChain(self):
-        self.ikStart = cmds.joint(n=self.metacarp + "_ikStart", rad=3)
-        self.ikEnd = cmds.joint(n=self.distal + "_ikEnd", rad=3)
-
-        cmds.matchTransform(self.ikStart, self.metacarp)
-        #cmds.makeIdentity(ikStart, self.metacarp)
-        cmds.matchTransform(self.ikEnd, self.distal)
-
-    def ikHandles(self):
+    """def ikHandles(self):
         fingerIKH = cmds.ikHandle(sj=self.ikStart, ee=self.ikEnd, sol="ikSCsolver", n=self.metacarp + "_ikHandle")[0]
-        return fingerIKH
+        return fingerIKH"""
 
-class ControllerFinger(Finger):
+class ControllerFinger(DriverFinger):
 
-    def __init__(self, metacarp, distal, fullLength):
-        super(ControllerFinger, self).__init__(metacarp, distal, fullLength)
-
+    def __init__(self, metacarp, distal, fullLength, metacarpDrv, distalDrv, hierarchyDrv):
+        super(ControllerFinger, self).__init__(metacarp, distal, fullLength, metacarpDrv, distalDrv, hierarchyDrv)
 
     def makeController(self):
         self.fingerLOCS = []
@@ -55,7 +36,6 @@ class ControllerFinger(Finger):
             cmds.matchTransform(boneLOC, self.fullLength[x])
         
         for x in range(len(self.fullLength[:-1])):
-            #print(x)
             cmds.parent(self.fingerLOCS[x+1], self.fingerLOCS[x])
             pass
 
@@ -63,31 +43,57 @@ class ControllerFinger(Finger):
         for LOC in self.fingerLOCS:
             cmds.color(LOC, rgb=(colorR, colorG, colorB))
 
-### Selection definition and the original chain hierarchy
-startJoint = cmds.ls(sl=1)[0]
-fullFinger = cmds.listRelatives(startJoint, ad=1, typ="joint")
-fullFinger.append(startJoint)
-fullFinger.reverse()
+def basic_finger():
+    ### Selection definition and the original chain hierarchy
+    startJoint = cmds.ls(sl=1)[0]
+    entireFinger = cmds.listRelatives(startJoint, ad=1, typ="joint")
+    entireFinger.append(startJoint)
+    entireFinger.reverse()
 
-# fullFinger[0] = first joint selected -> metacarp
-# fullFinger[-1] = last joint in the chain -> distal
-firstFinger = Finger(fullFinger[0], fullFinger[-1], fullFinger)
+    baseMetacarp = entireFinger[0] #First bone in a finger
+    baseDistal = entireFinger[-1] #Last bone in a finger
 
-driverFinger = DriverFinger(fullFinger[0], fullFinger[-1], fullFinger)
-driverFinger.duplicateChain(fullFinger[0] + "__nuovo", fullFinger[-1] + "__nuovo")
+    # baseMetacarp = first joint selected -> metacarp
+    # baseDistal = last joint in the chain -> distal
+    firstFinger = Finger(baseMetacarp, baseDistal, entireFinger)
+    return entireFinger
 
-driverFinger.connectToOrigin()
+def make_driver_chain():
+
+    fingerHierarchyDrv = []
+
+    entireFinger = basic_finger()
+    print("ciao --> ", entireFinger)
+
+    for bone in entireFinger:
+        boneDrv = cmds.joint(n=bone + "__nuovo")
+        fingerHierarchyDrv.append(boneDrv) 
+        cmds.matchTransform(boneDrv, bone)
+                
+    driverFinger = DriverFinger(baseMetacarp, baseDistal, entireFinger, 
+                                baseMetacarp + "__nuovo", baseDistal + "__nuovo", fingerHierarchyDrv)
+    cmds.parent(fingerHierarchyDrv[0], w=1) #Parent into world the driver chain
+    cmds.select(cl=1) #Maya automatically selects the new chain but I don't want
+    
+    driverFinger.connectToOrigin()
+
+    ikStart = cmds.joint(n=baseMetacarp + "_ikStart", rad=3)
+    ikEnd = cmds.joint(n=baseDistal + "_ikEnd", rad=3)
+
+    cmds.matchTransform(ikStart, baseMetacarp)
+    cmds.matchTransform(ikEnd, baseDistal)
 
 # Create ikHandle
-ikChain = driverFinger.ikChain()
-finger_ikHandles = driverFinger.ikHandles()
-# Queste tre righe qua sotto sono la morte della programmazione
-finger_ikHandles_grp = cmds.group(n="ik_grp", em=1)
+#finger_ikHandles = driverFinger.ikHandles()
+
+# Group ikHandles 
+"""finger_ikHandles_grp = cmds.group(n="ik_grp", em=1)
 cmds.parent(finger_ikHandles, "ik_grp")
 if finger_ikHandles_grp == "ik_grp1":
-    cmds.delete(finger_ikHandles_grp)
+    cmds.delete(finger_ikHandles_grp)"""
 
-createCtrl = ControllerFinger(fullFinger[0], fullFinger[-1], fullFinger)
+createCtrl = ControllerFinger(baseMetacarp, baseDistal, entireFinger, 
+                            baseMetacarp + "__nuovo", baseDistal + "__nuovo", fingerHierarchyDrv)
 createCtrl.makeController()
 
 # Change color based on side
@@ -95,3 +101,5 @@ if startJoint[0:2] == "l_":
     createCtrl.jointColor(0, 0, 255)
 elif startJoint[0:2] == "r_":
     createCtrl.jointColor(255, 0, 0)
+
+make_driver_chain()
