f94700a233e1c997cf2c83f47f5371818e17166e
ssimbox <ssimone0825@gmail.com>
2021-08-07 16:07:04 +0200

    chain definition in __init__

--- Auto Hand/README.md
+++ Auto Hand/README.md
@@ -1,31 +1,3 @@
-# AutoHand
-![Alt Text](https://media.giphy.com/media/NYyDRYhQClclSwf4Fh/giphy.gif)
-## Features 
-
-For now very simple stuff but it could be very useful to create an usable hand with fingers control.
-
-- Various length fingers
-- Fingers controller creation with attributes to control them
-
-P.S. Another rule: if you use what I call _supportJoint_ (watch gif second shot) please use this hierarchy
-
-- Hand
-    - Thumb
-    - SupportJoint
-        - other fingers
-
-## To-Do list
-
-- [x] IK Hand - Actually it started all for this, to create automatically an IK Hand. This is my main goal
-- [x] Various hand builds - My objective was to build the human hand (five fingers) but could be useful have three, four or six fingers.
-- [ ] Change attribute order
-- [ ] Delete some useless attributes
-
-## How to install
-Save *Auto_Hand* in your maya scripts folder
-Then, write and save this lines into your shelf
-
-```
-import Auto_Hand
-Auto_Hand.showUI()
-```
+Ho inserito dentro all'oggetto _Finger_ come trovarlo ma, ciò implica che qualsiasi cosa verrà selezionata allora sarà soggetta ad ogni tipo di trasformazione
+Tutto ciò si nota nel metodo *make_locators_attributes* che, automaticamente non riesce ad individuare la catena *_rig* ma, nel caso venga selezionata, il processo richiesto va a buon fine.
+A questo punto, sconsiglio di definire la catena madre dentro la classe *Finger* una volta per tutte
--- Auto Hand/sbx-hand.py
+++ Auto Hand/sbx-hand.py
@@ -4,8 +4,11 @@ import maya.cmds as cmds
 #  
 class BaseChain(object):
 
-    def __init__(self, hierarchy):
-        self.hierarchy = hierarchy
+    def __init__(self):
+        self.metacarp = cmds.ls(sl=1)[0]
+        self.hierarchy = cmds.listRelatives(self.metacarp, ad=1, typ="joint")
+        self.hierarchy.append(self.metacarp)
+        self.hierarchy.reverse()
 
     def bone_count(self):
         count = 0
@@ -21,12 +24,20 @@ class BaseChain(object):
 
 class DriverChain(BaseChain):
 
-    def __init__(self, hierarchy, hierarchyDrv):
-        super(DriverChain, self).__init__(hierarchy)
-        self.hierarchyDrv = hierarchyDrv
+    def __init__(self):
+        super(DriverChain, self).__init__()
+        self.hierarchyDrv = []
+
+    def add(self, bone):
+        self.hierarchyDrv.append(bone)
+        return self.hierarchyDrv
 
     # Directly connect driver chain with the base one
     def connect_to_baseFinger(self):
+        
+        print("hierarchy --> {}".format(self.hierarchy))
+        print("hierarchyDrv --> {}".format(self.hierarchyDrv))
+        
         for bone, boneDrv in zip(self.hierarchy, self.hierarchyDrv):
             cmds.parentConstraint(boneDrv, bone)
 
@@ -35,7 +46,7 @@ class Locators(DriverChain):
         super(DriverChain, self).__init__(hierarchy, hierarchyDrv)
         pass
 
-def chain_definition():
+"""def chain_definition():
     
     # Chain definition
     metacarp = cmds.ls(sl=1)[0]
@@ -44,47 +55,45 @@ def chain_definition():
     hierarchy.reverse()
 
     return hierarchy
-
+"""
 
 def make_driver_chain(*args):
 
-    hierarchy = chain_definition()
+    #hierarchy = chain_definition()
     
     # Istantiating BaseChain and printing number of joint in every single chain
-    original_chain = BaseChain(hierarchy)
+    original_chain = BaseChain()
     original_chain.bone_count()
-    
-    # Building up the driver hierarchy
-    driver_hierarchy = []
 
-    for bone in hierarchy:
+    driver_finger = DriverChain()
+
+    for bone in original_chain.hierarchy:
         boneDrv = cmds.joint(n=bone + "__rig")
-        driver_hierarchy.append(boneDrv)
+        driver_finger.add(boneDrv)
         cmds.matchTransform(boneDrv, bone)
 
-    cmds.parent(driver_hierarchy[0], w=1)
+    cmds.parent(driver_finger.hierarchyDrv[0], w=1)
 
     # Intantiating DriverChain and parentConstraint every driver joint with the relative original 
-    driver_finger = DriverChain(hierarchy, driver_hierarchy)
     driver_finger.connect_to_baseFinger()
 
-    return driver_hierarchy
 
 def make_ik_chain(*args):
 
-    hierarchy = chain_definition()
+    #hierarchy = chain_definition()
+    original_chain = BaseChain()
 
     # Creating a two-joint chain where gonna apply "reverse-hand" ik-handles
-    ikStart = cmds.joint(n=hierarchy[0] + "_ikStart", rad=3)
-    ikEnd = cmds.joint(n=hierarchy[-1] + "_ikEnd", rad=3)
+    ikStart = cmds.joint(n=original_chain.hierarchy[0] + "_ikStart", rad=3)
+    ikEnd = cmds.joint(n=original_chain.hierarchy[-1] + "_ikEnd", rad=3)
 
     # Parent this chain to the world using the original chain attributes
     cmds.parent(ikStart, w=1)
-    cmds.matchTransform(ikStart, hierarchy[0])
-    cmds.matchTransform(ikEnd, hierarchy[-1])
+    cmds.matchTransform(ikStart, original_chain.hierarchy[0])
+    cmds.matchTransform(ikEnd, original_chain.hierarchy[-1])
 
     # single chain ik handle build and parenting in a single grp
-    finger_ikHandle = cmds.ikHandle(sj=ikStart, ee=ikEnd, sol="ikSCsolver", n=hierarchy[0] + "_ikHandle")[0]
+    finger_ikHandle = cmds.ikHandle(sj=ikStart, ee=ikEnd, sol="ikSCsolver", n=original_chain.hierarchy[0] + "_ikHandle")[0]
     cmds.parent(finger_ikHandle, ik_hand_grp)
 
 # group automatically generating at the startup where parent fingers ik_handles
@@ -93,6 +102,22 @@ ik_hand_grp = cmds.group(n= "fingers_ik", em=1, w=1)
 
 def make_locators_attributes(*args):
     
+    
+    original_chain = BaseChain()
+    driver_finger = DriverChain()
+
+    fingerLOCS = []
+
+    print(original_chain.hierarchy)
+    print(driver_finger.hierarchyDrv)
+
+    for bone in driver_finger.hierarchy:
+        boneLOC = cmds.spaceLocator(n=bone + "_LOC")
+        fingerLOCS.append(boneLOC)
+        cmds.matchTransform(boneLOC, bone)
+        cmds.orientConstraint(boneLOC, bone)
+
+    """
     hierarchy = chain_definition()
     hierarchyDrv = make_driver_chain()
     print(hierarchyDrv)
@@ -107,11 +132,13 @@ def make_locators_attributes(*args):
 
     for x in range(len(hierarchy[:-1])):
             cmds.parent(fingerLOCS[x+1], fingerLOCS[x])
-
+            """
 # User Interface
 def showUI():
 
-    if cmds.window("switchModeUI", ex = 1): cmds.deleteUI("switchModeUI")
+    if cmds.window("switchModeUI", ex = 1): 
+        cmds.deleteUI("switchModeUI")
+        cmds.delete(ik_hand_grp)
     myWin = cmds.window("switchModeUI", t="sbx-autohand", w=300, h=300, s=1)
     mainLayout = cmds.formLayout(nd=50)
 
